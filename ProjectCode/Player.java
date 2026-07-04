cd65316a8878ee2498b9942248466dd821e714ec
Rafia <95438775+rafia23@users.noreply.github.com>
2026-07-04 21:17:43 +0600

    Add Player.java

---
+++ ProjectCode/Player.java
@@ -0,0 +1,22 @@
+//Player.java
+
+package ProjectCode;
+
+public class Player {
+
+private static String name;
+	
+	public Player(String s) {
+		name = s;
+	}
+	
+	public static void setName(String s) {
+		name = s;
+	}
+	
+	public static String getName() {
+		return name;
+	}
+}
+
+
